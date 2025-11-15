d0e03a83bb127386ee081e576d552657f0230251
Adhithyan2004 <adhibanugopan@gmail.com>
2025-11-15 15:32:52 +0530

    added CORS policy

--- server/main.py
+++ server/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from routes import spotify, ai
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 
 app.include_router(spotify.router, prefix="", tags=["Spotify"])
 app.include_router(ai.router, prefix="", tags=["AI"])
@@ -9,4 +21,4 @@ app.include_router(ai.router, prefix="", tags=["AI"])
 
 @app.get("/")
 def home():
-    return "What ra too much scene putting ah"
+    return "What ra sudeep too much scene putting ur"
